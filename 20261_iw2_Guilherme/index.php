cc9cafcebb2ea80eb11f6f03c8dac3f417ea38c3
gui-gomes96 <gelsongomes2020gel@gmail.com>
2026-05-27 11:36:34 -0300

    Bootstrap add

--- 20261_iw2_Guilherme/index.php
+++ 20261_iw2_Guilherme/index.php
@@ -4,7 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <title>Cadastro de Camisas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src = "npm i bootstrap@5.2.1"></script>
+    <link rel="stylesheet" href="https://bootstrapcdn.com">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 <script>
 $(document).ready(function () {
 
@@ -82,31 +86,52 @@ $(document).ready(function () {
 </script>
 </head>
 
-<body>
-    <h1>Registrar Camisetas</h1>
 
+<body>
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+Adicionar
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Adicionar Camiseta</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+                <p>Cor da Camiseta</p>
         <input type="text" id="Color" placeholder="Cor da camisa">
 
-        <select id="Tamanho">
-            <option value="P">P</option>
-            <option value="M">M</option>
-            <option value="G">G</option>
-            <option value="GG">GG</option>
-        </select>
-        
-        <button type="button" id="RegistrarBtn">Registrar</button>
+            <select id="Tamanho">
+                <option value="P">P</option>
+                <option value="M">M</option>
+                <option value="G">G</option>
+                <option value="GG">GG</option>
+            </select>
+                
+            <button type="button" class="btn btn-primary" id="RegistrarBtn">Registrar</button>
 
-        <!--<<button type="button" id="ExibirBtn1">Exibir</button> -->
-        <!--<button type="button" id="ExibirBtn2">Exibir 2</button> -->
 
-    <div id="Resposta">
-        <?php
-            include 'select.php';
-            echo exibir();
-        ?>
 
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Fechar</button>
+
+      </div>
     </div>
-     <!--<div id="Exibir2"></div> -->
+  </div>
+</div>
+        
+<div id="Resposta">
+    <?php
+        include 'select.php';
+         echo exibir();
+    ?>
+ </div>
 
 </body>
 </html>
--- 20261_iw2_Guilherme/select.php
+++ 20261_iw2_Guilherme/select.php
@@ -1,29 +1,30 @@
-<?php
-
-function exibir(){
-
-    include 'conecta.php';
-
-    $resultado = "<table border='1'>";
-
-    $stmt = $conn->query("SELECT * FROM tb_camisa");
-
-    while($row = $stmt->fetchObject()){
-
-        $resultado .= "
-        <tr>
-            <td>$row->cd_camisa</td>
-            <td>$row->cor</td>
-            <td>$row->tamanho</td>
-            <td>
-                <button class='deletar' id='$row->cd_camisa'>Excluir</button>
-                <button class='editar' id='$row->cd_camisa'>Editar</button>
-            </td>
-        </tr>";
-    }
-
-    $resultado .= "</table>";
-
-    return $resultado;
-}
+<?php 
+function exibir(){ 
+    include 'conecta.php'; 
+    
+    $resultado = '<table class="table table-bordered table-striped table-hover">'; 
+    $resultado .= '<thead class="thead-dark">
+                        <tr>
+                            <th>ID</th>
+                            <th>Cor</th>
+                            <th>Tamanho</th>
+                        </tr>
+                   </thead>';
+    $resultado .= '<tbody>';
+    $stmt = $conn->query("SELECT * FROM tb_camisa"); 
+    while($row = $stmt->fetchObject()){ 
+        $resultado .= '<tr> 
+                            <td>'. $row->cd_camisa .'</td> 
+                            <td>'. $row->cor .'</td> 
+                            <td>'. $row->tamanho .'</td> 
+                            <td> 
+                                <button class="btn btn-danger btn-sm deletar" id="'. $row->cd_camisa .'">Excluir</button> 
+                                <button class="btn btn-primary btn-sm editar" id="'. $row->cd_camisa .'">Editar</button> 
+                            </td> 
+                       </tr>'; 
+    } 
+    
+    $resultado .= '</tbody></table>'; 
+    return $resultado; 
+} 
 ?>
